3efc3913696c450294839b5c816cdd46cc0ada07
Irvin Jethro Velas <JethroV@ph.neolife.com>
2018-03-13 16:31:36 +0800

    Day3-Lab 8 – Add Data Access layer to the project
    Day3-Lab 8 – Add Data Access layer to the project

--- LearnMVC7Days/LearnMVC7Days/Models/EmployeeBusinessLayer.cs
+++ LearnMVC7Days/LearnMVC7Days/Models/EmployeeBusinessLayer.cs
@@ -3,32 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using LearnMVC7Days.DataAccessLayer;
+
 namespace LearnMVC7Days.Models
 {
     public class EmployeeBusinessLayer
     {
         public List<Employee> GetEmployees()
         {
-            List<Employee> employees = new List<Employee>();
-            Employee emp = new Employee();
-            emp.FirstName = "johnson";
-            emp.LastName = " fernandes";
-            emp.Salary = 14000;
-            employees.Add(emp);
-
-            emp = new Employee();
-            emp.FirstName = "michael";
-            emp.LastName = "jackson";
-            emp.Salary = 16000;
-            employees.Add(emp);
-
-            emp = new Employee();
-            emp.FirstName = "robert";
-            emp.LastName = " pattinson";
-            emp.Salary = 20000;
-            employees.Add(emp);
-
-            return employees;
+            SalesERPDAL salesDal = new SalesERPDAL();
+            return salesDal.Employees.ToList();
         }
     }
 }
